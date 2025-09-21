046d80fc1f6f25cbb2ed55ebc945db9c0b90b7d6
Ramdan Muhamad Hasan <132186158+Ramdan241004@users.noreply.github.com>
2025-09-21 22:53:42 +0700

    Add files via upload

---
+++ sketch.ino
@@ -0,0 +1,199 @@
+#include "secrets.h"
+#include <WiFiClientSecure.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include "WiFi.h"
+#include <DHT.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+
+#define AWS_IOT_PUBLISH_TOPIC "esp32/pub"
+
+#define DHT_PIN 15
+#define DHT_TYPE DHT22
+DHT dht(DHT_PIN, DHT_TYPE);
+
+Adafruit_MPU6050 mpu;
+
+WiFiClientSecure net = WiFiClientSecure();
+PubSubClient client(net);
+
+int lb = 0, ub = 50;
+
+void AdaFruitSetup()
+{
+    if (!mpu.begin())
+    {
+        Serial.println("Failed to find MPU6050 chip");
+        while (1)
+        {
+            delay(10);
+        }
+    }
+    Serial.println("MPU6050 Found!");
+
+    mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+    Serial.print("Accelerometer range set to: ");
+    switch (mpu.getAccelerometerRange())
+    {
+    case MPU6050_RANGE_2_G:
+        Serial.println("+-2G");
+        break;
+    case MPU6050_RANGE_4_G:
+        Serial.println("+-4G");
+        break;
+    case MPU6050_RANGE_8_G:
+        Serial.println("+-8G");
+        break;
+    case MPU6050_RANGE_16_G:
+        Serial.println("+-16G");
+        break;
+    }
+    mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+    Serial.print("Gyro range set to: ");
+    switch (mpu.getGyroRange())
+    {
+    case MPU6050_RANGE_250_DEG:
+        Serial.println("+- 250 deg/s");
+        break;
+    case MPU6050_RANGE_500_DEG:
+        Serial.println("+- 500 deg/s");
+        break;
+    case MPU6050_RANGE_1000_DEG:
+        Serial.println("+- 1000 deg/s");
+        break;
+    case MPU6050_RANGE_2000_DEG:
+        Serial.println("+- 2000 deg/s");
+        break;
+    }
+
+    mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+    Serial.print("Filter bandwidth set to: ");
+    switch (mpu.getFilterBandwidth())
+    {
+    case MPU6050_BAND_260_HZ:
+        Serial.println("260 Hz");
+        break;
+    case MPU6050_BAND_184_HZ:
+        Serial.println("184 Hz");
+        break;
+    case MPU6050_BAND_94_HZ:
+        Serial.println("94 Hz");
+        break;
+    case MPU6050_BAND_44_HZ:
+        Serial.println("44 Hz");
+        break;
+    case MPU6050_BAND_21_HZ:
+        Serial.println("21 Hz");
+        break;
+    case MPU6050_BAND_10_HZ:
+        Serial.println("10 Hz");
+        break;
+    case MPU6050_BAND_5_HZ:
+        Serial.println("5 Hz");
+        break;
+    }
+}
+
+void connectAWS()
+{
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+    Serial.println("Connecting to Wi-Fi");
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+
+    // Configure WiFiClientSecure to use the AWS IoT device credentials
+    net.setCACert(AWS_CERT_CA);
+    net.setCertificate(AWS_CERT_CRT);
+    net.setPrivateKey(AWS_CERT_PRIVATE);
+
+    // Connect to the MQTT broker on the AWS endpoint we defined earlier
+    client.setServer(AWS_IOT_ENDPOINT, 8883);
+
+    // Create a message handler
+    client.setCallback(messageHandler);
+
+    Serial.println("Connecting to AWS IOT");
+
+    while (!client.connect(THINGNAME))
+    {
+        Serial.print(".");
+        delay(100);
+    }
+
+    if (!client.connected())
+    {
+        Serial.println("AWS IoT Timeout!");
+        return;
+    }
+
+    Serial.println("AWS IoT Connected!");
+}
+
+void publishMessage()
+{
+    StaticJsonDocument<200> doc;
+
+    sensors_event_t a, g, temperature;
+    mpu.getEvent(&a, &g, &temperature);
+
+    float temp = dht.readTemperature();
+    float humidity = dht.readHumidity();
+    float accel_x = a.acceleration.x;
+    float accel_y = a.acceleration.y;
+    float accel_z = a.acceleration.z;
+    float gyro_x = g.gyro.x;
+    float gyro_y = g.gyro.y;
+    float gyro_z = g.gyro.z;
+
+    if (isnan(temp) || isnan(humidity))
+    {
+        Serial.println("Failed to read from DHT sensor");
+        return;
+    }
+
+    doc["accel_x"] = accel_x;
+    doc["accel_y"] = accel_y;
+    doc["accel_z"] = accel_z;
+    doc["gyro_x"] = gyro_x;
+    doc["gyro_y"] = gyro_y;
+    doc["gyro_z"] = gyro_z;
+    doc["humidity"] = humidity;
+    doc["temp"] = temp;
+    char jsonBuffer[512];
+    serializeJson(doc, jsonBuffer); // print to client
+
+    client.publish(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
+}
+
+void messageHandler(char *topic, byte *payload, unsigned int length)
+{
+    Serial.print("incoming: ");
+    Serial.println(topic);
+
+    StaticJsonDocument<200> doc;
+    deserializeJson(doc, payload);
+    const char *message = doc["message"];
+    Serial.println(message);
+}
+
+void setup()
+{
+    Serial.begin(115200);
+    AdaFruitSetup();
+    dht.begin();
+    connectAWS();
+}
+
+void loop()
+{
+    publishMessage();
+    client.loop();
+    delay(1000);
+}
